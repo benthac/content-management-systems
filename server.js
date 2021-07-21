c150245949e4a0acd647651c46db38f9f29d434b
ben thacker <benthac@gmail.com>
2021-07-21 19:22:10 -0400

    added initial route

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+app.listen(PORT, () => {
+    console.log(`running on port ${PORT}`);
+});
