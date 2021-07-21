35d72e81f9109b00b0ec3e51b653f75f7aab4084
ben thacker <benthac@gmail.com>
2021-07-21 19:27:29 -0400

    database connected

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,16 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: "08June28",
+        database: 'employee'
+    },
+    console.log('Success!')
+);
 
 
 
