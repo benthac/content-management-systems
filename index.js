2aac074e78ee20c2d1d0358010528ce201d27800
ben thacker <benthac@gmail.com>
2021-07-22 18:43:16 -0400

    added adds

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const questions = [
             'View all roles',
             'View all employees',
             'Add a department',
-            'Add a roll',
+            'Add a role',
             'Add an employee',
             'Update an employee roll',
             'Quit'
@@ -22,19 +22,56 @@ const questions = [
 ]
 
 function viewDepartments() {
-db.promise().query(`SELECT * FROM department`).then(([rows, fields]) => {
+    db.promise().query(`SELECT * FROM department`).then(([rows, fields]) => {
     console.table(rows);
-}).then(init())};
+    }).then(init())};
 
 function viewRoles() {
-db.promise().query(`SELECT * FROM role`).then(([rows, fields]) => {
+    db.promise().query(`SELECT * FROM role`).then(([rows, fields]) => {
     console.table(rows);
-}).then(init())};
+    }).then(init())};
 
 function viewEmployees() {
-db.promise().query(`SELECT * FROM employee`).then(([rows, fields]) => {
+    db.promise().query(`SELECT * FROM employee`).then(([rows, fields]) => {
     console.table(rows);
-}).then(init())};
+    }).then(init())};
+
+function addDepartment() {
+    let sql = `INSERT INTO department (id, name)
+        VALUES (?,?)`;
+    let params = [];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(result);
+    });
+};
+
+function addRoles() {
+    let sql = `INSERT INTO role (id, title, salary, department)
+        VALUES (?,?,?,?)`;
+    let params = [];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(result);
+    });
+};
+
+function addEmployee() {
+    let sql = `INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+    VALUES (?,?,?,?,?)`;
+let params = [];
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+};
 
 function init() {
     inquirer.prompt(questions).then((answers) => {
